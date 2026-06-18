6c297ddab8412358e749a9cfaee24224549bef4e
Gloxiniaaa <hnm142758@gmail.com>
2026-06-18 10:35:23 +0700

    [unit2] player controller & pickups

---
+++ Assets/Unit2/Scripts/CameraController.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+
+public class CameraController : MonoBehaviour {
+    [SerializeField] private Transform _target;
+    private Vector3 _offset;
+
+    void Start()
+    {
+        _offset = transform.position - _target.position;
+    }
+
+    void LateUpdate()
+    {
+        transform.position = _target.position + _offset;
+    }
+
+}
--- Assets/Unit2/Scripts/PlayerController.cs
+++ Assets/Unit2/Scripts/PlayerController.cs
@@ -1,16 +1,50 @@
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class PlayerController : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    // Rigidbody of the player.
+    private Rigidbody _rb;
+
+    // Movement along X and Y axes.
+    private float _movementX;
+    private float _movementY;
+
+    // Speed at which the player moves.
+    [SerializeField] private float _speed = 0;
+
+    // Start is called before the first frame update.
     void Start()
     {
-        
+        // Get and store the Rigidbody component attached to the player.
+        _rb = GetComponent<Rigidbody>();
     }
 
-    // Update is called once per frame
-    void Update()
+    // This function is called when a move input is detected.
+    void OnMove(InputValue movementValue)
     {
-        
+        // Convert the input value into a Vector2 for movement.
+        Vector2 movementVector = movementValue.Get<Vector2>();
+
+        // Store the X and Y components of the movement.
+        _movementX = movementVector.x;
+        _movementY = movementVector.y;
+    }
+
+    // FixedUpdate is called once per fixed frame-rate frame.
+    void FixedUpdate()
+    {
+        // Create a 3D movement vector using the X and Y inputs.
+        Vector3 movement = new Vector3(_movementX, 0.0f, _movementY);
+
+        // Apply force to the Rigidbody to move the player.
+        _rb.AddForce(movement * _speed);
+    }
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Pickup"))
+        {
+            other.gameObject.SetActive(false);
+        }
     }
 }
