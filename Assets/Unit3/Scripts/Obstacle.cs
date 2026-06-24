cf81bbab320f9ff507cc0c43096d9f25f25a2076
Gloxiniaaa <hnm142758@gmail.com>
2026-06-24 15:59:37 +0700

    [unit6] addd impact vfx

--- Assets/Unit3/Scripts/Obstacle.cs
+++ Assets/Unit3/Scripts/Obstacle.cs
@@ -1,12 +1,15 @@
+using System.Collections;
 using UnityEngine;
 
 public class Obstacle : MonoBehaviour
 {
+    private static WaitForSeconds _waitForSeconds0_5 = new WaitForSeconds(0.5f);
     [SerializeField] private float _minSize = 0.5f;
     [SerializeField] private float _maxSize = 2.0f;
     [SerializeField] private float _minSpeed = 50f;
     [SerializeField] private float _maxSpeed = 150f;
     [SerializeField] private AudioClip _impactSfx;
+    [SerializeField] private GameObject _impactVfx;
 
     Rigidbody2D _rb;
     AudioSource _as;
@@ -26,6 +29,18 @@ public class Obstacle : MonoBehaviour
 
     void OnCollisionEnter2D(Collision2D collision)
     {
+        if (transform.localScale.x < collision.transform.localScale.x)
+            return;
         _as.PlayOneShot(_impactSfx);
+        Vector3 connectVector = collision.transform.position - transform.position;
+        Vector3 impactPoint = transform.position + connectVector.normalized * transform.localScale.x;
+        GameObject vfx = Instantiate(_impactVfx, impactPoint, Quaternion.identity);
+        StartCoroutine(DelayDestroyObject(vfx));
+    }
+
+    IEnumerator DelayDestroyObject(GameObject obj)
+    {
+        yield return _waitForSeconds0_5;
+        Destroy(obj);
     }
 }
